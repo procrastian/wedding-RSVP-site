afafdf6bac13dca5ec787c90d15bd4b27b34de52
Your Name <your_email@address>
2024-02-05 14:26:41 +0000

    date handling library added

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
-
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
 
 export async function getStaticProps({ params }) {
   // Add the "await" keyword like this:
@@ -24,11 +25,14 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
